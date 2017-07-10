1559e1efbc744902ac23f17a8d50645ddccdb373
TPei <t.s.peikert@gmail.com>
2017-07-10 13:43:07 +0200

    also call #to_json on parameters to convert them to json

--- app/workers/infection_schedule_worker.rb
+++ app/workers/infection_schedule_worker.rb
@@ -10,7 +10,7 @@ class InfectionScheduleWorker
     Unirest.post(
       "#{ENV['MAIN_SERVICE_URL']}/game/infection",
       headers: { "content-type" => "application/json" },
-      parameters: { game_id: game_id }
+      parameters: { game_id: game_id }.to_json
     )
 
     InfectionScheduleWorker.perform_in(time.seconds, game_id, time)
